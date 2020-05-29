814e9ea7b6b0c0561f7a242e1edc4c86886832fc
Liang Liu <liul85@gmail.com>
2020-05-29 13:58:28 +1000

    added user and login

---
+++ Application/User/Login.cs
@@ -0,0 +1,66 @@
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Errors;
+using Domain;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.User
+{
+    public class Login
+    {
+        public class Query : IRequest<User>
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+
+        }
+
+        public class QueryValidator : AbstractValidator<Query>
+        {
+            public QueryValidator()
+            {
+                RuleFor(user => user.Email).NotEmpty();
+                RuleFor(user => user.Password).NotEmpty();
+            }
+        }
+
+        public class Handler : IRequestHandler<Query, User>
+        {
+            private readonly UserManager<AppUser> _userManager;
+            private readonly SignInManager<AppUser> _signInManager;
+            public Handler(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+            {
+                this._signInManager = signInManager;
+                this._userManager = userManager;
+            }
+
+            public async Task<User> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var user = await this._userManager.FindByEmailAsync(request.Email);
+                if (user == null)
+                {
+                    throw new RestException(HttpStatusCode.Unauthorized);
+                }
+
+                var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+
+                if (result.Succeeded)
+                {
+                    //TODO generate token
+                    return new User
+                    {
+                        DisplayName = user.DisplayName,
+                        Token = "not yet",
+                        Username = user.UserName,
+                        Image = null
+                    };
+                }
+
+                throw new RestException(HttpStatusCode.Unauthorized);
+            }
+        }
+    }
+}
